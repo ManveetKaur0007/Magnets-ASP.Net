16f8468c2f6430964387ed0038198ad2c14f96ed
ManveetKaur0007 <w0782002@myscc.ca>
2022-02-24 16:15:56 -0500

    recent changes

--- ManveetKaurMagnets/Controllers/ManveetMagnetsController.cs
+++ ManveetKaurMagnets/Controllers/ManveetMagnetsController.cs
@@ -8,18 +8,27 @@ namespace ManveetKaurMagnets.Controllers
         // 
         // GET: /ManveetMagnets/
 
-        public string Index()
+        //public string Index()    // this will only return Index of type string
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            //return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /ManveetMagnets/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        /*public string Welcome(string name, int ID = 1)
         {
             //return "This is the Welcome action method...";
             return HtmlEncoder.Default.Encode($"Hello {name}, ID {ID}");
+        }*/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
