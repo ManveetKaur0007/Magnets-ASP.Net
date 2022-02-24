75c9baf267298bb043bc380df60c327ee6ad3cfb
ManveetKaur0007 <w0782002@myscc.ca>
2022-02-24 15:31:26 -0500

    Add project files.

---
+++ ManveetKaurMagnets/Controllers/ManveetMagnetsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ManveetKaurMagnets.Controllers
+{
+    public class ManveetMagnetsController : Controller
+    {
+        // 
+        // GET: /ManveetMagnets/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /ManveetMagnets/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            //return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID {ID}");
+        }
+    }
+}
---
+++ ManveetKaurMagnets/README.txt
@@ -0,0 +1,11 @@
+﻿Author Name: Manveet Kaur
+Program Name: ManveetKaurMagnets
+Student ID: W0782002
+Date created: 2022/02/24
+Purpose of the project: To learn about creating ASP.NET Core MVC application
+
+02:47PM
+Started the project and made the base of the project 
+Added the ReadMe.txt file to write step by step process of the project
+jumping on 2nd part
+ 
